5eb6bda9df5586fca11194888032b99de3f57a02
Gaymard <33988017+Erffa@users.noreply.github.com>
2020-04-27 01:36:46 +0200

    Create exemple.py

---
+++ tutos/exemple.py
@@ -0,0 +1,14 @@
+myvar = "Hello in there"
+
+def myfunction(n):
+  return 3*n+1
+  
+class MyClass:
+  def __init__(self, arg):
+    self.arg = arg
+    return
+    
+  def display(self):
+    print("Here is the class")
+    print("You gave the arg {}".format(self.arg))
+    return
